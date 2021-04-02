f76b51bdad28c1d33876dd541c1c17faf72a534f
Eric <contreras.eric18@gmail.com>
2021-04-02 15:29:53 -0400

    commit tree 72d534398f183d17f9b97a2013d69bf2ce73cca8

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    first = self.first_name
+    last = self.last_name
+    first + " " + last 
+  end 
+
+  def list_roles
+    self.characters.collect do |character|
+        "#{character.name} - #{character.show.name}"
+    end.join
+  end 
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list 
+        self.characters.collect do |character|
+            if character.show == self
+                character.actor.full_name
+            end
+        end
+    end 
+
 end
